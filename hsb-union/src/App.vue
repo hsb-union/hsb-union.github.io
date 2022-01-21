dd4605db98206777a7061581b82ca110222da6eb
miathedev <mia@metzler.systems>
2022-01-21 23:23:52 +0100

    strange bootstrap

--- hsb-union/src/App.vue
+++ hsb-union/src/App.vue
@@ -5,6 +5,22 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
+  <div class="dropdown">
+    <button
+        class="btn btn-secondary dropdown-toggle"
+        type="button"
+        id="dropdownMenuButton1"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+    >
+      Check Bootstrap
+    </button>
+    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+      <li><a class="dropdown-item" href="#">Action</a></li>
+      <li><a class="dropdown-item" href="#">Another action</a></li>
+      <li><a class="dropdown-item" href="#">Something else here</a></li>
+    </ul>
+  </div>
   <img alt="Vue logo" src="http://placekitten.com/300/300" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
--- hsb-union/src/main.js
+++ hsb-union/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import "popper.js"
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
