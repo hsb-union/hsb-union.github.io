6fe110314fd9ecd0b5f5100c6a4a4c1757e9ca54
miathedev <mia@metzler.systems>
2022-01-21 23:02:09 +0100

    Move to vite

--- hsb-union/src/main.js
+++ hsb-union/src/main.js
@@ -1,13 +1,4 @@
-import '@babel/polyfill'
-import 'mutationobserver-shim'
-import Vue from 'vue'
-import './plugins/bootstrap-vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
 
-Vue.config.productionTip = false
-
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+createApp(App).mount('#app')
--- hsb-union/vue.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    publicPath: 'https://hsb-union.github.io/'
-}
